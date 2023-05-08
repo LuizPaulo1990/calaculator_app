6c3e6227dd590681053b0ef4df23216f182086ca
Luiz Paulo Martins dos Santos <117921451+LuizPaulo1990@users.noreply.github.com>
2023-05-08 19:17:46 -0300

    add code

---
+++ main.js
@@ -0,0 +1,30 @@
+let display = document.getElementById('display');
+
+
+
+
+function adicionar(value){
+    display.innerText += value;
+    console.log(display);
+  
+}
+
+function limpar(){
+    display.innerText ='';
+    console.log(display);
+}
+
+function ponto(value){
+    display.innerHTML += value;
+}
+
+function operadores(value){
+    display.innerText += value; 
+}
+
+function resultado(){
+    
+}
+
+
+    
